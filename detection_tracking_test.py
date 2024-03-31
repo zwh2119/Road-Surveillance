f0bccfb4a1cabeab9897eca7a5104a387f751300
onecheck <1548767361@qq.com>
2024-04-01 01:18:56 +0800

    add optical flow tracking

--- detection_tracking_test.py
+++ detection_tracking_test.py
@@ -107,13 +107,14 @@ def calculate_map(predictions, ground_truths, iou_threshold=0.5):
     return mAP
 
 
-def detection_tracking_delay_test():
-    detector_trt = detection_trt({
-        'weights': '/home/nvidia/zwh/Auto-Edge/batch_test/yolov5s_batch1.engine',
-        'plugin_library': '/home/nvidia/zwh/Auto-Edge/batch_test/libbatch1plugins.so',
-        'batch_size': 1,
-        'device': 0
-    })
+def detection_tracking_delay_test(detector_trt=None, resolution=None):
+    if not detector_trt:
+        detector_trt = detection_trt({
+            'weights': '/home/nvidia/zwh/Auto-Edge/batch_test/yolov5s_batch1.engine',
+            'plugin_library': '/home/nvidia/zwh/Auto-Edge/batch_test/libbatch1plugins.so',
+            'batch_size': 1,
+            'device': 0
+        })
 
     # cfg = get_config()
     # cfg.merge_from_file('car_tracking/simple/configs/deep_sort.yaml')
@@ -154,14 +155,24 @@ def detection_tracking_delay_test():
         pic_path = os.path.join(video_dir, info[0])
         prev_frame = frame
         frame = cv2.imread(pic_path)
-        # frame = cv2.resize(frame, (480, 360))
-        print(f'frame size:{frame.shape}')
+        if resolution:
+            raw_length = frame.shape[1]
+            raw_height = frame.shape[0]
+            new_length = resolution[0]
+            new_height = resolution[1]
+            frame = cv2.resize(frame, resolution)
+        # print(f'frame size:{frame.shape}')
         # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
         bbox_gt = [float(b) for b in info[1:]]
         boxes_gt = np.array(bbox_gt, dtype=np.float32).reshape(-1, 4)
         frame_gt = []
         for box in boxes_gt.tolist():
+            if resolution:
+                box[0] *= new_length / raw_length
+                box[1] *= new_height / raw_height
+                box[2] *= new_length / raw_length
+                box[3] *= new_height / raw_height
             frame_gt.append({'bbox': box, 'class': 1})
 
         if result_detection and prob:
@@ -188,7 +199,7 @@ def detection_tracking_delay_test():
         response = detector_trt([frame])
         result_detection = response['result'][0]
         prob = response['probs'][0]
-        print(f'object number: {len(result_detection)}')
+        # print(f'object number: {len(result_detection)}')
         prediction_detection = []
         for box, score in zip(result_detection, prob):
             prediction_detection.append({'bbox': box, 'prob': score, 'class': 1})
@@ -200,6 +211,7 @@ def detection_tracking_delay_test():
     print(f'【detector】 delay:{np.mean(detector_trt_delay):.4f}s    map:{np.mean(detector_trt_acc)}')
     # print(f'【tracker】  simple:{np.mean(tracker_simple_delay):.4f}s')
     print(f'【tracker】  delay:{np.mean(tracker_trt_delay):.4f}s    map:{np.mean(tracker_trt_acc)}')
+    return np.mean(detector_trt_delay), np.mean(detector_trt_acc), np.mean(tracker_trt_delay), np.mean(tracker_trt_acc)
 
 
 def batch_delay_test():
@@ -350,7 +362,58 @@ def batch_delay_test_baseline():
         json.dump({'delay': batch_delay, 'acc': batch_acc}, f)
 
 
+def detection_tracking_resolution_test():
+    detector_trt = detection_trt({
+        'weights': '/home/nvidia/zwh/Auto-Edge/batch_test/yolov5s_batch1.engine',
+        'plugin_library': '/home/nvidia/zwh/Auto-Edge/batch_test/libbatch1plugins.so',
+        'batch_size': 1,
+        'device': 0
+    })
+
+    resolution_dict = {
+        "HVGA_360p(4:3)": (480, 360),
+        "nHD_360p(16:9)": (640, 360),
+
+        "VGA_480p(4:3)": (640, 480),
+
+        "SVGA_600p(4:3)": (800, 600),
+
+        "qHD_540p(16:9)": (960, 540),
+
+        "DVCPRO-HD_720p(4:3)": (960, 720),  # 691200
+        "HD_720p(16:9)": (1280, 720),  # 921600
+        "WallpaperHD_720p(18:9)": (1440, 720),  # 1036800
+
+        "WXGA_800p(16:10)": (1280, 800),  # 1024000
+        "QuadVGA_960p(4:3)": (1280, 960),  # 1228800
+        "WXGA+_900p(16:10)": (1440, 900),  # 1296000
+        "FWXGA+_960p(3:2)": (1440, 960),  # 1382400
+        "HD+_900p(16:9)": (1600, 900),  # 1440000
+
+        "DVCPRO-HD_1080p(16:9)": (1440, 1080),  # 1555200
+        "FHD_1080p(16:9)": (1920, 1080)  # 2073600
+    }
+    detection_delay_list = []
+    detection_acc_list = []
+    tracker_delay_list = []
+    tracker_acc_list = []
+    for resolution_name in resolution_dict:
+        resolution = resolution_dict[resolution_name]
+        print(f'start test on resolution of {resolution}..')
+        detection_delay, detection_acc, tracker_delay, tracker_acc = detection_tracking_delay_test(detector_trt, resolution)
+        detection_delay_list.append(detection_delay)
+        detection_acc_list.append(detection_acc)
+        tracker_delay_list.append(tracker_delay)
+        tracker_acc_list.append(tracker_acc)
+    with open('detection_tracking_resolution.json', 'w') as f:
+        json.dump({'detection_delay':detection_delay_list,
+                   'detection_acc':detection_acc_list,
+                   'tracker_delay':tracker_delay_list,
+                   'tracker_acc':tracker_acc_list},f)
+
+
 if __name__ == '__main__':
     # detection_tracking_delay_test()
-    batch_delay_test()
+    # batch_delay_test()
     # batch_delay_test_baseline()
+    detection_tracking_resolution_test()
